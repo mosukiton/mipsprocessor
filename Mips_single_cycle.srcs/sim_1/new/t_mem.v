e23a691a47c411919d7a15db1e775d79f73f22db
Harmohan Sahota <harmohan.ss@live.co.uk>
2017-07-13 17:09:17 +0100

    Initialised the clk and wrote tests and verified memoryaccess module

--- Mips_single_cycle.srcs/sim_1/new/t_mem.v
+++ Mips_single_cycle.srcs/sim_1/new/t_mem.v
@@ -44,12 +44,41 @@ module t_mem;
         .ZerowireM( zerowire ), .clk( clk )
     );
 
+    // Intialise the clock
+    initial begin
+        clk = 0;
+        forever begin
+            #10 clk = 1;
+            #10 clk = 0;
+        end
+    end
+
     initial begin
         
         // Initialise inputs
         writedata = 0; aluresultin = 0; pcbranch1 = 0;
         writereg1 = 0;
         branch = 0; memwrite = 0; memtoreg1 = 0; regwrite1 = 0; zerowire = 0; clk = 0;
+
+        // Wait 100 ns for global resets
+        #100;
+
+        // Test zerowire + branch and PCBranch
+        #10 zerowire = 0; branch = 0; pcbranch1 = 32'h00000008; //expected output pcsrc = 0; pcbranch2 = 32'h00000008;
+        #20 zerowire = 0; branch = 1; pcbranch1 = 32'h00000008; //expected output pcsrc = 0; pcbranch2 = 32'h00000008;
+        #20 zerowire = 1; branch = 0; pcbranch1 = 32'h00000008; //expected output pcsrc = 0; pcbranch2 = 32'h00000008;
+        #20 zerowire = 1; branch = 1; pcbranch1 = 32'h00000008; //expected output pcsrc = 1; pcbranch2 = 32'h00000008;
+
+        // Test ALUResultIn for when MemWrite = 0 and MemWrite = 1;
+        #20 aluresultin = 32'h00000001; memwrite = 1; writedata = 32'hFFFFFFFF;
+        // expected output aluresultout = 32'h00000001; dmem stores value FFFFFFFF into address 32'h00000001
+        #20 aluresultin = 32'h00000001; memwrite = 0; writedata = 32'hAAAAAAAA; // expected output aluresultout = 32'h00000001; readdata = 32'hFFFFFFFF;
+
+        // Test read function of dmem
+
+        // Test follow-through signals
+        #20 writereg1 = 5'b01000; memtoreg1 = 1; regwrite1 = 1; // expected output writereg2 = 5'b01000; memtoreg2 = 1; regwrite2 = 1;
+        #20 writereg1 = 5'b01001; memtoreg1 = 0; regwrite1 = 0; // expected output writereg2 = 5'b01001; memtoreg2 = 0; regwrite2 = 0;
 
     end
 
